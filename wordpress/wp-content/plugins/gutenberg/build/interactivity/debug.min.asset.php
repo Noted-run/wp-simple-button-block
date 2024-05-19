571baaae7cb84cd57449a63808577fff76b59286
soray677 <soray677@outlook.com>
2024-05-19 12:13:49 +0900

    wordpressを管理下から外す

--- wordpress/wp-content/plugins/gutenberg/build/interactivity/debug.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'f8b25652acbe23e9ac1d', 'type' => 'module');
--- wordpress/wp-content/plugins/gutenberg/build/interactivity/index.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '8a28f6cac13665a0c502', 'type' => 'module');

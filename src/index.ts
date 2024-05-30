c2ffdae2574844322c575959c084e7a5fe9a7561
soray677 <soray677@outlook.com>
2024-05-30 09:09:05 +0900

    iconを追加

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,13 @@ import save from "./save";
 import metadata from "./block.json";
 import { ImgType } from "./type";
 import "./index.css";
+// @ts-ignore
+import { ReactComponent as Icon } from "./icon.svg";
 
 registerBlockType(metadata.name, {
 	title: metadata.title,
 	category: metadata.category,
+	icon: Icon,
 	attributes: {
 		title: {
 			type: "string",
